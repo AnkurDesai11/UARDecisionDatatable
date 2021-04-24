ab60332d5054fbb0c968f054e994f68683a26be7
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2021-04-24 20:45:46 +0530

    added dependencies

--- enterDecision.php
+++ enterDecision.php
@@ -7,3 +7,20 @@ if ( ! isset($_SESSION["name"]) ) {
     error_log("ACCESS DENIED-Not logged in");
 }
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Access Review</title>
+    <link href="tabulator-master/dist/css/tabulator.min.css" rel="stylesheet">
+    <script type="text/javascript" src="tabulator-master/dist/js/tabulator.min.js"></script>
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.6.0.slim.js" type="text/javascript"></script>
+    <script>
+        window.jQuery || document.write('<script src="jquery-3.6.0.slim.min"><\/script>'))
+    </script>
+
+</head>
