5d1571eddef7dc11d3d4ed6f039793851754bf75
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2021-04-24 16:16:21 +0530

    Added enterDecison page

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if ( isset($_POST['email']) && isset($_POST['pass']) ) {
     if ( $row === FALSE ) {
         $_SESSION['error'] = "Invalid Email and Password combination";
         error_log("Login failed as password is incorrect for ".$_POST['email']);
-        header("Location: login.php");
+        header("Location: index.php");
         return;
     } else { 
         $_SESSION['success'] = "Login success";
